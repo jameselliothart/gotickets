ef60d7039af3d589fcf1d5619c249c52d44148a3
James Hart <james.elliot.hart@gmail.com>
2020-12-28 11:29:42 -0600

    creating tickets

--- src/tickets/ticket.go
+++ src/tickets/ticket.go
@@ -1,10 +1,32 @@
 package tickets
 
+import "github.com/lithammer/shortuuid/v3"
+
+type TicketID struct {
+	ID string `json:"id"`
+}
+
 type Ticket struct {
-	ID      string `json:"id"`
-	Summary string `json:"summary"`
+	TicketID
+	Summary  string `json:"summary"`
+}
+
+type CreateTicketDto struct {
+	Summary string
+}
+
+func NewTicketID() TicketID {
+	return TicketID{ID: shortuuid.New()}
+}
+
+func NewTicket(summary string) Ticket {
+	return Ticket{
+		TicketID: NewTicketID(),
+		Summary: summary,
+	}
 }
 
 type DataHandler interface {
 	GetTickets() []Ticket
+	CreateTicket(CreateTicketDto) (TicketID, error)
 }
